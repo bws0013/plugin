aca65223c710f094c7dade298ca3271037590799
Ben <i@auburn.edu>
2017-08-06 10:40:27 -0500

    Working struct example

--- src/client/client.go
+++ src/client/client.go
@@ -1,97 +1,32 @@
 package main
 
 import (
-  "net"
-  "fmt"
-  "bufio"
-  "os"
-  "io/ioutil"
-  "path/filepath"
-  "encoding/gob"
+    "fmt"
+    "log"
+    "net"
+    "encoding/gob"
 )
-//
 
-type my_packet struct {
-  Message string
-  Contains_file bool
-  File_name string
-  File []byte
+type P struct {
+    M, N int64
 }
-
 
 func main() {
-  //dial_server_message()
-
-  p := form_packet("hello", "./../../storage/sent/numbers.in")
-
-  // connect to this socket
-  dial_server_packet(p)
-  fmt.Println(p.Message)
-}
-
-func dial_server_packet(packet my_packet) {
-  conn, err := net.Dial("tcp", "127.0.0.1:8081")
-
-  if err != nil {
-    fmt.Println("Unable to send!")
-    return
-  }
-
-  encoder := gob.NewEncoder(conn)
-  err = encoder.Encode(packet)
-  check_err(err, "everything is fine")
-  conn.Close()
-
-  message, _ := bufio.NewReader(conn).ReadString('\n')
-  fmt.Println("Message from server: " + message)
-
-
-}
-
-func dial_server_message() {
-  conn, _ := net.Dial("tcp", "127.0.0.1:8081")
-
-  for {
-    // read in input from stdin
-    reader := bufio.NewReader(os.Stdin)
-    fmt.Print("Text to send: ")
-    text, _ := reader.ReadString('\n')
-    // send to socket
-    fmt.Fprintf(conn, text + "\n")
-    // listen for reply
-    message, _ := bufio.NewReader(conn).ReadString('\n')
-    fmt.Print("Message from server: "+message)
-  }
-}
-
-func form_packet(message, file_path string) my_packet {
-  file_exists := check_for_file(file_path)
-  if file_exists {
-    file, err := ioutil.ReadFile(file_path)
-    file_name := filepath.Base(file_path)
-    check_err(err, "")
-    return my_packet{message, file_exists, file_name, file}
-  } else {
-    return my_packet{message, file_exists, "", nil}
-  }
-
+    fmt.Println("start client");
+    conn, err := net.Dial("tcp", "127.0.0.1:8081")
+    if err != nil {
+        log.Fatal("Connection error", err)
+    }
+    encoder := gob.NewEncoder(conn)
+    p := &P{1, 2}
+    encoder.Encode(p)
+    conn.Close()
+    fmt.Println("done");
 }
 
 func check_err(err error, message string) {
     if err != nil {
-      panic(err)
+        panic(err)
     }
-    if len(message) != 0 {
-      fmt.Printf("%s\n", message)
-    }
-}
-
-func check_for_file(file_path string) bool {
-  finfo, err := os.Stat(file_path)
-  check_err(err, "")
-  if !finfo.IsDir() {
-    //fmt.Println(file_path)
-    return true
-  }
-  return false
+    fmt.Printf("%s\n", message)
 }
--- src/server/server.go
+++ src/server/server.go
@@ -1,109 +1,42 @@
 package main
 
 import (
-  "net"
-  "fmt"
-  "bufio"
-  "strings"
+    "fmt"
+    "net"
+    "encoding/gob"
 )
-import "encoding/gob"
 
-// TODO make a struct for handling file transfers
-
-type my_packet struct {
-  Message string
-  Contains_file bool
-  File_name string
-  File []byte
+type P struct {
+    M, N int64
+}
+func handleConnection(conn net.Conn) {
+    dec := gob.NewDecoder(conn)
+    p := &P{}
+    err := dec.Decode(p)
+    check_err(err, "Decoding")
+    fmt.Printf("Received : %+v\n", p);
+    fmt.Printf("M : %d\n", p.M);
+    conn.Close()
 }
 
 func main() {
-  //listen_message()
-  //listen_packet()
-  // for i := 0; i < 2; i++ {
-  //   go listen()
-  // }
-
-  fmt.Println("Launching server...")
-  ln, err := net.Listen("tcp", ":8081")
-  check(err, "Server is ready.")
-  for {
-    conn, err := ln.Accept()
-    check(err, "Accepted connection.")
-    go listen_packet(conn) // a goroutine handles conn so that the loop can accept other connections
-  }
-
-}
-
-func listen_packet(conn net.Conn) {
-
-  // dec := gob.NewDecoder(conn)
-  //   p := &P{}
-  //   dec.Decode(p)
-  //   fmt.Printf("Received : %+v", p);
-  //   conn.Close()
-
-  dec := gob.NewDecoder(conn)
-  p := &my_packet{}
-  err := dec.Decode(p)
-  check(err, "No problems on read in")
-
-  fmt.Println("Message: %s", p.File_name)
-
-
-  if dec != nil {
-    fmt.Printf("Client disconnected.\n")
-    return
-  }
-
-  // conn.Write([]byte("liftoff" + "\n"))
-
-      // dec := gob.NewDecoder(conn)
-      // p := &my_packet{}
-      // dec.Decode(p)
-      //
-      // fmt.Println("Message: %s", p.message)
-      //
-      // // send new string back to client
-      // conn.Write([]byte("Message Recieved: " + p.message + "\n"))
-
-}
-
-func listen_message() {
-  fmt.Println("Launching server...")
-
-  // listen on all interfaces
-  ln, err := net.Listen("tcp", ":8081")
-  check(err, "Server is ready.")
-
-  // run loop forever (or until ctrl-c)
-  for {
-    conn, _ := ln.Accept()
-    check(err, "Accepted connection.")
-
-    go func() {
-      // will listen for message to process ending in newline (\n)
-      buf := bufio.NewReader(conn)
-
-      for {
-        message, err := buf.ReadString('\n')
+    fmt.Println("start");
+   ln, err := net.Listen("tcp", ":8081")
+    if err != nil {
+      check_err(err, "Listening!")
+        // handle error
+    }
+    for {
+        conn, err := ln.Accept() // this blocks until connection or error
         if err != nil {
-          fmt.Printf("Client disconnected.\n")
-          break
+            check_err(err, "Accepting!")
+            continue
         }
-
-        // output message received
-        fmt.Print("Message Received:", string(message))
-        // sample process for string received
-        newmessage := strings.ToUpper(message)
-        // send new string back to client
-        conn.Write([]byte(newmessage + "\n"))
-      }
-    }()
-  }
+        go handleConnection(conn) // a goroutine handles conn so that the loop can accept other connections
+    }
 }
 
-func check(err error, message string) {
+func check_err(err error, message string) {
     if err != nil {
         panic(err)
     }
